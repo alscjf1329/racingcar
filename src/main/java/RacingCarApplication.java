ad692a96694c088fc33ebc66cb8694bf6c1e119b
jiwondev <sylvester7412@gmail.com>
2022-11-06 18:08:14 +0900

    build: 프로젝트 초기화

---
+++ src/main/java/RacingCarApplication.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public final class RacingCarApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello " + Arrays.toString(args));
+    }
+}
---
+++ src/test/java/RacingCarApplicationTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingCarApplicationTest {
+    @Test
+    void 테스트() {
+        String[] strings = {"안녕", "친구들"};
+        RacingCarApplication.main(strings);
+    }
+
+}

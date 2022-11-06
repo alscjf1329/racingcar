4361ea15dd0de6f2c7369ff74a64bfa297138548
wonindev <sangwonparkdev@gmail.com>
2022-11-06 22:06:05 +0900

    상원

--- src/main/java/RacingCarApplication.java
+++ src/main/java/RacingCarApplication.java
@@ -3,15 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public final class RacingCarApplication {
     public static void main(String[] args) throws IOException {
         System.out.println("Hello " + Arrays.toString(args));
-        //d
+        Scanner sc = new Scanner(System.in);
         BufferedReader brForInputName = new BufferedReader(new InputStreamReader(System.in));
         CarListCreate carlist = new CarListCreate();
         List<String> carNameList = carlist.createList(brForInputName.readLine());
+
+
+
         System.out.println(carNameList);
+
+
     }
 }
 
